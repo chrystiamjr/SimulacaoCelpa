d6957f17226da26f8735eb53437b70161b94e8c1
Chrystiam Nascimento Junior <chrystiamjr@gmail.com>
2016-12-16 17:30:34 -0300

    v 0.3.3 - Modification on login page

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 
 <style>
 	#overlay {
-		background: rgba(255,255,255,0.6);
+		background: rgba(255, 255, 255, 0.6);
 		color: #666666;
 		position: fixed;
 		height: 100%;
@@ -45,7 +45,7 @@
 	}
 
 	body {
-		background: linear-gradient(rgba(0, 0, 0, 0.69), rgba(0, 0, 0, 0.69)),url('img/bg.jpg') no-repeat center center fixed;
+		background: linear-gradient(rgba(0, 0, 0, 0.69), rgba(0, 0, 0, 0.69)), url('img/bg.jpg') no-repeat center center fixed;
 		-webkit-background-size: cover;
 		-moz-background-size: cover;
 		-o-background-size: cover;
@@ -71,7 +71,8 @@
 					<div class="form-group">
 						<label class="control-label col-md-2" for="cpfLogin" style="text-align: center">CPF:</label>
 						<div class="col-lg-9">
-							<input type="text" class="form-control" id="cpfLogin" pattern=".{14,}" required maxlength="14" OnKeyPress="formatar('###.###.###-##', this)" placeholder="CPF">
+							<input type="text" class="form-control" id="cpfLogin" pattern=".{14,}" required maxlength="14"
+							       OnKeyPress="formatar('###.###.###-##', this)" placeholder="CPF">
 						</div>
 					</div>
 					<!-- Password -->
@@ -84,7 +85,7 @@
 					<!-- Remember me checkbox and sign in button -->
 					<div class="form-group">
 						<div class="pull-right" style="margin-right: 60px">
-							<div class="checkbox" >
+							<div class="checkbox">
 								<label>
 									<input type="checkbox"> Lembre de mim
 								</label>
@@ -96,42 +97,102 @@
 			</div>
 			<div class="widget-foot">
 				<button type="submit" class="btn btn-primary" id="loginSistema">Entar no sistema</button>
-				<button type="reset" class="btn btn-default pull-right">Limpar campos</button>
+				<button type="reset" class="btn btn-default pull-right" id="limpaDados">Limpar campos</button>
 			</div>
 		</div>
 
 	</div>
 </div>
 
-<?php include 'includes/scripts.php'?>
+<?php include 'includes/scripts.php' ?>
 
 <script>
-	$("#loginSistema").click(function(){
-		var cpf = $('#cpfLogin').val();
-		cpf = cpf.split('.').join("");
-		cpf = cpf.split('-').join("");
-		var senha = $('#inputPassword').val();
-		$.ajax({
-			url: '/SimulacaoCelpa/database/dbUsuario.php',
-			type: 'post',
-			data: {'cpf': cpf, 'senha': senha, 'action': 'login'},
-			datatype: 'json',
-			start: function(){
-				var loading = '<div id="overlay"><img src="img/spinner/loader.gif" alt="Loading" /><br/></div>';
-				$('#loader').append(loading);
-			},
-			success: function(data){
-				$('#loader#overlay').remove();
-				if(data == 1){
-					window.location.href = window.location.href+"view/Dashboard/dash2.php";
-				}else{
-					alert('CPF e senha inválidos ou inexistentes');
-					$('#cpfLogin').val("");
-					$('#inputPassword').val("");
+	$('#limpaDados').click(function () {
+		$('#cpfLogin').val('');
+		$('#inputPassword').val('');
+	});
+
+	$("#loginSistema").click(function () {
+		$('#cpfLogin').removeClass('label-danger');
+		$('#inputPassword').removeClass('label-danger');
+		if($('#cpfLogin').val() != "" && $('#inputPassword').val() != "") {
+			var cpf = $('#cpfLogin').val();
+			cpf = cpf.split('.').join("");
+			cpf = cpf.split('-').join("");
+			var senha = $('#inputPassword').val();
+			$.ajax({
+				url: '/SimulacaoCelpa/database/dbUsuario.php',
+				type: 'post',
+				data: {'cpf': cpf, 'senha': senha, 'action': 'login'},
+				datatype: 'json',
+				start: function () {
+					var loading = '<div id="overlay"><img src="img/spinner/loader.gif" alt="Loading" /><br/></div>';
+					$('#loader').append(loading);
+				},
+				success: function (data) {
+					$('#loader#overlay').remove();
+					if (data == 1) {
+						window.location.href = window.location.href + "view/Dashboard/dash2.php";
+					} else {
+						alert('CPF e senha inválidos ou inexistentes');
+						$('#cpfLogin').val("");
+						$('#inputPassword').val("");
+					}
 				}
-			}
-		});
+			});
+		} else{
+			alert('Os campos não podem ficar em branco!');
+			$('#cpfLogin').addClass('label-danger');
+			$('#inputPassword').addClass('label-danger');
+		}
 	})
+
+//	$(document).keypress(function (event) {
+//		var keycode = event.keyCode || event.which;
+//		if (keycode == '13') {
+//			alert('You pressed a "enter" key in somewhere');
+//		}
+//	});
+
+	$(document).keypress(function (event) {
+		$('#cpfLogin').removeClass('label-danger');
+		$('#inputPassword').removeClass('label-danger');
+		var keycode = event.keyCode || event.which;
+		if (keycode == '13') {
+			if($('#cpfLogin').val() != "" && $('#inputPassword').val() != "") {
+				var cpf = $('#cpfLogin').val();
+				cpf = cpf.split('.').join("");
+				cpf = cpf.split('-').join("");
+				var senha = $('#inputPassword').val();
+				$.ajax({
+					url: '/SimulacaoCelpa/database/dbUsuario.php',
+					type: 'post',
+					data: {'cpf': cpf, 'senha': senha, 'action': 'login'},
+					datatype: 'json',
+					start: function () {
+						var loading = '<div id="overlay"><img src="img/spinner/loader.gif" alt="Loading" /><br/></div>';
+						$('#loader').append(loading);
+					},
+					success: function (data) {
+						$('#loader#overlay').remove();
+						if (data == 1) {
+							window.location.href = window.location.href + "view/Dashboard/dash2.php";
+						} else {
+							alert('CPF e senha inválidos ou inexistentes');
+							$('#cpfLogin').val("");
+							$('#inputPassword').val("");
+						}
+					}
+				});
+			} else{
+				alert('Os campos não podem ficar em branco!');
+				$('#cpfLogin').addClass('label-danger');
+				$('#inputPassword').addClass('label-danger');
+			}
+		}
+	});
+
+
 </script>
 
 </body>
