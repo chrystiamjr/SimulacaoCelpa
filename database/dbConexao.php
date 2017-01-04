b91f6f36f58645a88aaaca57f58deddca12c9054
André Souza <atmdsouza@gmail.com>
2017-01-04 12:15:18 -0300

    Arquivo Git Ignore

--- database/dbConexao.php
+++ database/dbConexao.php
@@ -2,7 +2,7 @@
 
 function conectar (){
 	try{
-		$conn = new PDO("mysql:dbname=db_equatorial;host=localhost;charset=utf8", "root", "root");
+		$conn = new PDO("mysql:dbname=db_equatorial;host=localhost;charset=utf8", "root", "emage");
 		return $conn;
 		// echo "Sucesso";
 	} catch(PDOException $e) {
